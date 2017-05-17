deba5d4960c0f2b720682ef9cbd9bffaff8b37c6
JackArthurton <jack.arthurton@simplybusiness.co.uk>
2017-05-17 13:19:52 +0100

    lec 64

--- Scala-and-Spark-Bootcamp/Machine_Learning_Sections/Classification/LogRegProject.scala
+++ Scala-and-Spark-Bootcamp/Machine_Learning_Sections/Classification/LogRegProject.scala
@@ -26,21 +26,35 @@
 //////////////////////
 
 // Import SparkSession and Logistic Regression
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.classification.LogisticRegression
 
 // Optional: Use the following code below to set the Error reporting
+import org.apache.log4j._
+Logger.getLogger("org").setLevel(Level.ERROR)
 
 // Create a Spark Session
-
+val spark = SparkSession.builder().getOrCreate()
 // Use Spark to read in the Advertising csv file.
-
+val df = spark.read.option("header","true").option("inferSchema","true").format("csv").load("advertising.csv")
 // Print the Schema of the DataFrame
+
+df.printSchema()
 
 ///////////////////////
 /// Display Data /////
 /////////////////////
 
 // Print out a sample row of the data (multiple ways to do this)
-
+val colnames = df.columns
+val firstrow = df.head(1)(0)
+println("\n")
+println("Example Data Row")
+for(ind <- Range(1,colnames.length)){
+  println(colnames(ind))
+  println(firstrow(ind))
+  println("\n")
+}
 
 ////////////////////////////////////////////////////
 //// Setting Up DataFrame for Machine Learning ////
@@ -51,16 +65,27 @@
 //    - Grab the following columns "Daily Time Spent on Site","Age","Area Income","Daily Internet Usage","Timestamp","Male"
 //    - Create a new column called Hour from the Timestamp containing the Hour of the click
 
+val timedata = df.withColumn("Hour",hour(df("Timestamp")))
+
+val logdata = (timedata.select(timedata("Clicked on Ad").as("label")
+                ,$"Hour",$"Daily Time Spent on Site",$"Age",$"Area Income"
+                ,$"Daily Internet Usage",$"Male")
+              )
 
 
 // Import VectorAssembler and Vectors
-
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
 // Create a new VectorAssembler object called assembler for the feature
 // columns as the input Set the output column to be called features
+val assembler = (new VectorAssembler().setInputCols(Array("Hour"
+                ,"Daily Time Spent on Site","Age","Area Income"
+                ,"Daily Internet Usage","Male")).setOutputCol("features")
+                )
 
 
 // Use randomSplit to create a train test split of 70/30
-
+val Array(training, test) = logdata.randomSplit(Array(0.7, 0.3), seed = 12345)
 
 ///////////////////////////////
 // Set Up the Pipeline ///////
@@ -68,22 +93,26 @@
 
 // Import Pipeline
 // Create a new LogisticRegression object called lr
+import org.apache.spark.ml.Pipeline
 
+val lr = new LogisticRegression()
 // Create a new pipeline with the stages: assembler, lr
-
+val pipeline = new Pipeline().setStages(Array(assembler,lr))
 // Fit the pipeline to training set.
-
+val model = pipeline.fit(training)
 
 // Get Results on Test Set with transform
-
+val results = model.transform(test)
 ////////////////////////////////////
 //// MODEL EVALUATION /////////////
 //////////////////////////////////
 
 // For Metrics and Evaluation import MulticlassMetrics
-
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 // Convert the test results to an RDD using .as and .rdd
-
+val predictionAndLabels = results.select($"prediction",$"label").as[(Double, Double)].rdd
 // Instantiate a new MulticlassMetrics object
-
+val metrics = new MulticlassMetrics(predictionAndLabels)
 // Print out the Confusion matrix
+println("Confusion Matrix:")
+println(metrics.confusionMatrix)
--- Scala-and-Spark-Bootcamp/Machine_Learning_Sections/Regression/LinRegExercise.scala
+++ Scala-and-Spark-Bootcamp/Machine_Learning_Sections/Regression/LinRegExercise.scala
@@ -4,23 +4,40 @@
 /////////////////////////////////////////
 
 // Import LinearRegression
-
+import org.apache.spark.ml.regression.LinearRegression
 // Optional: Use the following code below to set the Error reporting
 import org.apache.log4j._
 Logger.getLogger("org").setLevel(Level.ERROR)
 
 
 // Start a simple Spark Session
-
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession.builder().getOrCreate()
 // Use Spark to read in the Ecommerce Customers csv file.
+val data = spark.read.option("header","true").option("inferSchema","true").format("csv").load("Clean-Ecommerce.csv")
+
+
 
 // Print the Schema of the DataFrame
-
+data.printSchema()
 
 // Print out an example Row
 // Various ways to do this, just
 // choose whichever way you prefer
+data.head
 
+
+val colnames: Array[String] = data.columns
+val firstrow = data.head
+println("\n")
+println("Example Data Row")
+
+
+for(ind<- Range(1,colnames.length)){
+  println(colnames(ind))
+  println(firstrow(ind))
+  println("\n")
+}
 
 ////////////////////////////////////////////////////
 //// Setting Up DataFrame for Machine Learning ////
@@ -31,10 +48,14 @@ Logger.getLogger("org").setLevel(Level.ERROR)
 // ("label","features")
 
 // Import VectorAssembler and Vectors
+// into a single column of an array of feature values
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
 
 // Rename the Yearly Amount Spent Column as "label"
 // Also grab only the numerical columns from the data
 // Set all of this as a new dataframe called df
+val df = data.select(data("Yearly Amount Spent").as("label"),$"Avg Session Length", $"Time on App", $"Time on Website", $"Length of Membership")
 
 // An assembler converts the input values to a vector
 // A vector is what the ML algorithm reads to train a model
@@ -43,23 +64,30 @@ Logger.getLogger("org").setLevel(Level.ERROR)
 // to a single output column of an array called "features"
 // Set the input columns from which we are supposed to read the values.
 // Call this new object assembler
-
+val assembler = new VectorAssembler().setInputCols(Array("Avg Session Length", "Time on App", "Time on Website", "Length of Membership")).setOutputCol("features")
 // Use the assembler to transform our DataFrame to the two columns: label and features
-
+val output =  assembler.transform(df).select($"label",$"features")
 
 // Create a Linear Regression Model object
-
+val lr = new LinearRegression()
 
 // Fit the model to the data and call this model lrModel
-
+val lrModel = lr.fit(output)
 
 // Print the coefficients and intercept for linear regression
-
+println(s"Coefficients: ${lrModel.coefficients}")
+println(s"Intercept: ${lrModel.intercept}")
 // Summarize the model over the training set and print out some metrics!
 // Use the .summary method off your model to create an object
 // called trainingSummary
+val trainingSummary = lrModel.summary
+
 
 // Show the residuals, the RMSE, the MSE, and the R^2 Values.
+trainingSummary.residuals.show()
 
+println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+println(s"MSE: ${trainingSummary.meanSquaredError}")
+println(s"r2: ${trainingSummary.r2}")
 
 // Great Job!
